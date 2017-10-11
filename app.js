0a18c9679905ffc37096485107b8d3d4975d07d2
Your Namejudsonfisher <judsonefisher@gmail.com>
2017-10-11 15:27:09 -0500

    more added

---
+++ app.js
@@ -0,0 +1,7 @@
+// Create initial variables that take in functions and user commands
+var word = require("./word.js");
+var letter = require("./letter.js");
+var guessesRemaining = 6;
+var guess = process.argv[2];
+
+// Create 
